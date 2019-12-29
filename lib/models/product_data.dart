3f33ab880385545cd0c7e7454400d7dd627291dd
Sanjit Sarkar <ssanjit856@gmail.com>
2019-12-30 00:35:05 +0530

    Finished designing Home Page

---
+++ lib/models/product_data.dart
@@ -0,0 +1,7 @@
+class Product_Data {
+  String product_img;
+  String product_name;
+  int price;
+  String desc;
+  Product_Data({this.product_img, this.product_name, this.price,this.desc});
+}
---
+++ lib/pages/product.dart
@@ -0,0 +1,17 @@
+import 'package:async/async.dart';
+import 'package:flutter/material.dart';
+import '../models/product_data.dart';
+class Product extends StatefulWidget {
+  Product({Key key}) : super(key: key);
+
+  _ProductState createState() => new _ProductState();
+}
+
+class _ProductState extends State<Product> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+       
+    );
+  }
+}
